86bd53dcfe58b264d5b4263b465ccb70705a58b7
Rawisara Chairat <rawisara.chat@gmail.com>
2026-04-05 01:43:29 -0400

    fixes more on query_service.py

--- modules/query_service.py
+++ modules/query_service.py
@@ -2,7 +2,7 @@ import sqlite3
 import pandas as pd
 
 from modules.data_loader import load_data
-from schema_manager import append_csv_to_table, get_existing_table, create_table, resolve_schema, infer_schema
+from modules.schema_manager import append_csv_to_table, get_existing_table, create_table, resolve_schema, infer_schema
 
 # List all tables in the database.
 def list_tables(db_path):
